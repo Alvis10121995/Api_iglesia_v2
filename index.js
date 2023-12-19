23b981e6e328cdd5fedc7130305b48701024838d
AlvisAtencio <alvis.atencio3@gmail.com>
2023-12-18 19:56:05 -0500

    se agrega ruta para obtener video de api de youtube

--- index.js
+++ index.js
@@ -105,6 +105,19 @@ app.get('/instagram', async (req, res) => {
 });
 
 
+app.get('/youtube', async (req, res) => {
+  try {
+    const respuesta = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${process.env.key2}&channelId=${process.env.id2}&part=snippet,id&order=date&maxResults=20`);
+    const datos = await respuesta.json();
+       res.json(datos);
+  } catch (error) {
+     console.error('Error al consumir la API:', error.message);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+
+
 
 
 const server = app.listen(PORT, () => {
